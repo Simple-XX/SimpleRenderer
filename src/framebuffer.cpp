f5609899b6d34ca5b8599948393895d1e1d6ccb9
Zone.N <Zone.Niuzh@hotmail.com>
2022-09-03 22:07:27 +0800

    ref(framebuffer): update
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

---
+++ src/framebuffer.cpp
@@ -0,0 +1,114 @@
+
+/**
+ * @file framebuffer.cpp
+ * @brief 缓冲区抽象
+ * @author Zone.N (Zone.Niuzh@hotmail.com)
+ * @version 1.0
+ * @date 2022-09-03
+ * @copyright MIT LICENSE
+ * https://github.com/Simple-XX/SimpleRenderer
+ * @par change log:
+ * <table>
+ * <tr><th>Date<th>Author<th>Description
+ * <tr><td>2022-09-03<td>Zone.N<td>创建文件
+ * </table>
+ */
+
+#include "memory"
+#include "cassert"
+#include "framebuffer.h"
+
+framebuffer_t::framebuffer_t(void) {
+    width        = 0;
+    height       = 0;
+    color_buffer = nullptr;
+    depth_buffer = nullptr;
+    return;
+}
+
+framebuffer_t::framebuffer_t(uint32_t _width, uint32_t _height) {
+    width        = _width;
+    height       = _height;
+    color_buffer = new color_t[width * height];
+    depth_buffer = new depth_buffer_t[width * height];
+    return;
+}
+
+framebuffer_t::framebuffer_t(const framebuffer_t &_framebuffer) {
+    assert(width == _framebuffer.width);
+    assert(height == _framebuffer.height);
+    if (color_buffer == nullptr) {
+        color_buffer = new color_t[width * height];
+    }
+    if (depth_buffer == nullptr) {
+        depth_buffer = new depth_buffer_t[width * height];
+    }
+    if (_framebuffer.color_buffer != nullptr) {
+        memcpy(color_buffer, _framebuffer.color_buffer, width * height * BPP);
+    }
+    if (_framebuffer.depth_buffer != nullptr) {
+        memcpy(depth_buffer, _framebuffer.depth_buffer,
+               width * height * sizeof(depth_buffer_t));
+    }
+    return;
+}
+
+framebuffer_t::~framebuffer_t(void) {
+    if (color_buffer != nullptr) {
+        delete[] color_buffer;
+    }
+    if (depth_buffer != nullptr) {
+        delete[] depth_buffer;
+    }
+    return;
+}
+
+framebuffer_t &framebuffer_t::operator=(const framebuffer_t &_framebuffer) {
+    assert(&_framebuffer == this);
+    assert(width == _framebuffer.width);
+    assert(height == _framebuffer.height);
+    if (color_buffer == nullptr) {
+        color_buffer = new color_t[width * height];
+    }
+    if (depth_buffer == nullptr) {
+        depth_buffer = new depth_buffer_t[width * height];
+    }
+    if (_framebuffer.color_buffer != nullptr) {
+        memcpy(color_buffer, _framebuffer.color_buffer, width * height * BPP);
+    }
+    if (_framebuffer.depth_buffer != nullptr) {
+        memcpy(depth_buffer, _framebuffer.depth_buffer,
+               width * height * sizeof(depth_buffer_t));
+    }
+    return *this;
+}
+
+void framebuffer_t::clear(void) {
+    for (uint32_t i = 0; i < height; i++) {
+        for (uint32_t j = 0; j < width; j++) {
+            color_buffer[i * width + j] = 0;
+        }
+    }
+    return;
+}
+
+void framebuffer_t::clear(const color_t &_color) {
+    for (uint32_t i = 0; i < height; i++) {
+        for (uint32_t j = 0; j < width; j++) {
+            color_buffer[i * width + j] = _color;
+        }
+    }
+    return;
+}
+
+void framebuffer_t::pixel(int _x, int _y, const color_t &_color) {
+    color_buffer[_y * width + _x] = _color;
+    return;
+}
+
+framebuffer_t::color_t framebuffer_t::RGBA(const uint8_t _r, const uint8_t _g,
+                                           const uint8_t _b, const uint8_t _a) {
+    color_t color;
+    color = _r << 24 | _g << 16 | _b << 8 | _a;
+    return color;
+}
---
+++ src/include/framebuffer.h
@@ -0,0 +1,113 @@
+
+/**
+ * @file framebuffer.h
+ * @brief 缓冲区抽象
+ * @author Zone.N (Zone.Niuzh@hotmail.com)
+ * @version 1.0
+ * @date 2022-09-03
+ * @copyright MIT LICENSE
+ * https://github.com/Simple-XX/SimpleRenderer
+ * @par change log:
+ * <table>
+ * <tr><th>Date<th>Author<th>Description
+ * <tr><td>2022-09-03<td>Zone.N<td>创建文件
+ * </table>
+ */
+
+#ifndef _FRAMEBUFFER_H_
+#define _FRAMEBUFFER_H_
+
+#include "cstdint"
+
+/**
+ * @brief 缓冲
+ */
+class framebuffer_t {
+private:
+    /// 窗口宽度
+    uint32_t width;
+    /// 窗口高度
+    uint32_t height;
+
+    /// 颜色类型，格式为 rgba
+    typedef uint32_t color_t;
+    /// 颜色缓存
+    color_t *color_buffer;
+    /// 每像素字节数
+    static constexpr const uint8_t BPP = sizeof(color_t);
+
+    /// 深度类型
+    typedef float depth_buffer_t;
+    /// 深度缓存
+    depth_buffer_t *depth_buffer;
+
+public:
+    /// 默认宽度
+    static constexpr const uint32_t WIDTH = 1920;
+    /// 默认高度
+    static constexpr const uint32_t HEIGHT = 1080;
+
+    /**
+     * @brief 构造函数
+     */
+    framebuffer_t(void);
+
+    /**
+     * @brief 构造函数
+     * @param  _width           宽度
+     * @param  _height          高度
+     * @param  _color_type      颜色类型
+     */
+    framebuffer_t(uint32_t _width = WIDTH, uint32_t _height = HEIGHT);
+
+    /**
+     * @brief 拷贝构造
+     * @param  _framebuffer     另一个 framebuffer_t
+     */
+    framebuffer_t(const framebuffer_t &_framebuffer);
+
+    /**
+     * @brief 析构函数
+     */
+    ~framebuffer_t(void);
+
+    /**
+     * @brief 赋值
+     * @param  _framebuffer     另一个 framebuffer_t
+     * @return framebuffer_t&     结果
+     */
+    framebuffer_t &operator=(const framebuffer_t &_framebuffer);
+
+    /**
+     * @brief 清空
+     */
+    void clear(void);
+
+    /**
+     * @brief 清空成指定颜色
+     * @param  _color           指定颜色
+     */
+    void clear(const color_t &_color);
+
+    /**
+     * @brief 设置像素
+     * @param  _x               横向坐标
+     * @param  _y               纵向坐标
+     * @param  _color           颜色
+     * @note (0, 0) 在屏幕左上角
+     */
+    void pixel(int _x, int _y, const color_t &_color);
+
+    /**
+     * @brief 生成 rgba
+     * @param  _r               红
+     * @param  _g               绿
+     * @param  _b               蓝
+     * @param  _a               alpha
+     * @return color_t          rgba 颜色
+     */
+    static color_t RGBA(const uint8_t _r, const uint8_t _g, const uint8_t _b,
+                 const uint8_t _a) ;
+};
+
+#endif /* _FRAMEBUFFER_H_ */
