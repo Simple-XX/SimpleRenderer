a61ff6bb572f2fcf3ce336eecd01a59bd6c000b4
Zone.Niuzh <Zone.Niuzh@hotmail.com>
2023-08-22 14:53:06 +0800

    Add 3rd.cmake and doc
    Signed-off-by: Zone.Niuzh <Zone.Niuzh@hotmail.com>

---
+++ cmake/3rd.cmake
@@ -0,0 +1,149 @@
+
+# This file is a part of Simple-XX/SimpleRenderer
+# (https://github.com/Simple-XX/SimpleRenderer).
+#
+# 3rd.cmake for Simple-XX/SimpleRenderer.
+# 依赖管理
+
+# 设置依赖下载路径
+set(CPM_SOURCE_CACHE ${CMAKE_SOURCE_DIR}/3rd)
+# 优先使用本地文件
+set(CPM_USE_LOCAL_PACKAGES True)
+# https://github.com/cpm-cmake/CPM.cmake
+# -------- get_cpm.cmake --------
+set(CPM_DOWNLOAD_VERSION 0.38.2)
+
+if (CPM_SOURCE_CACHE)
+    set(CPM_DOWNLOAD_LOCATION "${CPM_SOURCE_CACHE}/cpm/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
+elseif (DEFINED ENV{CPM_SOURCE_CACHE})
+    set(CPM_DOWNLOAD_LOCATION "$ENV{CPM_SOURCE_CACHE}/cpm/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
+else ()
+    set(CPM_DOWNLOAD_LOCATION "${CMAKE_BINARY_DIR}/cmake/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
+endif ()
+
+# Expand relative path. This is important if the provided path contains a tilde (~)
+get_filename_component(CPM_DOWNLOAD_LOCATION ${CPM_DOWNLOAD_LOCATION} ABSOLUTE)
+
+function(download_cpm)
+    message(STATUS "Downloading CPM.cmake to ${CPM_DOWNLOAD_LOCATION}")
+    file(DOWNLOAD
+            https://github.com/cpm-cmake/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake
+            ${CPM_DOWNLOAD_LOCATION}
+    )
+endfunction()
+
+if (NOT (EXISTS ${CPM_DOWNLOAD_LOCATION}))
+    download_cpm()
+else ()
+    # resume download if it previously failed
+    file(READ ${CPM_DOWNLOAD_LOCATION} check)
+    if ("${check}" STREQUAL "")
+        download_cpm()
+    endif ()
+    unset(check)
+endif ()
+
+include(${CPM_DOWNLOAD_LOCATION})
+# -------- get_cpm.cmake --------
+
+# # https://github.com/google/googletest
+# CPMAddPackage(
+#   NAME googletest
+#   GITHUB_REPOSITORY google/googletest
+#   GIT_TAG v1.13.0
+#   VERSION 1.13.0
+#   OPTIONS
+#       "INSTALL_GTEST OFF"
+#       "gtest_force_shared_crt ON"
+# )
+
+# # https://github.com/abumq/easyloggingpp
+# CPMAddPackage(
+#   NAME easylogingpp
+#   VERSION 9.97.0
+#   GITHUB_REPOSITORY amrayn/easyloggingpp
+#   OPTIONS
+#   "build_static_lib ON"
+#   "lib_utc_datetime ON"
+# )
+
+# # https://github.com/freetype/freetype
+# CPMAddPackage(
+#   NAME freetype
+#   GIT_REPOSITORY https://github.com/freetype/freetype.git
+#   GIT_TAG VER-2-13-0
+#   VERSION 2.13.0
+# )
+# if (freetype_ADDED)
+#   add_library(Freetype::Freetype ALIAS freetype)
+# endif()
+
+# https://github.com/gdbinit/Gdbinit
+CPMAddPackage(
+        NAME gdbinit
+        GIT_REPOSITORY https://github.com/gdbinit/Gdbinit.git
+        GIT_TAG e5138c24226bdd05360ca41743d8315a9e366c40
+        DOWNLOAD_ONLY True
+)
+if (gdbinit_ADDED)
+    add_custom_target(gdbinit
+            COMMENT "Generate gdbinit ..."
+            WORKING_DIRECTORY ${gdbinit_SOURCE_DIR}
+            # 复制到根目录下并重命名
+            COMMAND
+            ${CMAKE_COMMAND}
+            -E
+            copy
+            ${gdbinit_SOURCE_DIR}/gdbinit
+            ${CMAKE_SOURCE_DIR}/.gdbinit
+    )
+endif ()
+
+# https://github.com/cpm-cmake/CPMLicenses.cmake
+# 保持在 CPMAddPackage 的最后
+CPMAddPackage(
+        NAME CPMLicenses.cmake
+        GITHUB_REPOSITORY cpm-cmake/CPMLicenses.cmake
+        VERSION 0.0.7
+)
+if (CPMLicenses.cmake_ADDED)
+    cpm_licenses_create_disclaimer_target(
+            write-licenses "${CMAKE_CURRENT_SOURCE_DIR}/3rd/LICENSE" "${CPM_PACKAGES}"
+    )
+endif ()
+# make 时自动在 3rd 文件夹下生成 LICENSE 文件
+add_custom_target(3rd_licenses
+        ALL
+        COMMAND
+        make
+        write-licenses
+)
+
+# doxygen
+find_package(Doxygen
+        REQUIRED dot)
+if (NOT DOXYGEN_FOUND)
+    message(FATAL_ERROR "Doxygen not found.\n"
+            "Following https://www.doxygen.nl/index.html to install.")
+endif ()
+
+# cppcheck
+find_program(CPPCHECK_EXE NAMES cppcheck)
+if (NOT CPPCHECK_EXE)
+    message(FATAL_ERROR "cppcheck not found.\n"
+            "Following https://cppcheck.sourceforge.io to install.")
+endif ()
+
+# clang-tidy
+find_program(CLANG_TIDY_EXE NAMES clang-tidy)
+if (NOT CLANG_TIDY_EXE)
+    message(FATAL_ERROR "clang-tidy not found.\n"
+            "Following https://clang.llvm.org/extra/clang-tidy to install.")
+endif ()
+
+# clang-format
+find_program(CLANG_FORMAT_EXE NAMES clang-format)
+if (NOT CLANG_FORMAT_EXE)
+    message(FATAL_ERROR "clang-format not found.\n"
+            "Following https://clang.llvm.org/docs/ClangFormat.html to install.")
+endif ()
