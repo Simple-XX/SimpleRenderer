f87c253bb854b8624c594df7538cf14a28352583
Zone.N <Zone.Niuzh@hotmail.com>
2023-09-20 11:10:22 +0800

    spdlog
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

---
+++ src/log.cpp
@@ -0,0 +1,19 @@
+
+/**
+ * @file log.cpp
+ * @brief 日志封装
+ * @author Zone.N (Zone.Niuzh@hotmail.com)
+ * @version 1.0
+ * @date 2022-09-14
+ * @copyright MIT LICENSE
+ * https://github.com/Simple-XX/SimpleRenderer
+ * @par change log:
+ * <table>
+ * <tr><th>Date<th>Author<th>Description
+ * <tr><td>2022-09-14<td>Zone.N<td>创建文件
+ * </table>
+ */
+
+#include <log.h>
+
+
