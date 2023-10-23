f45ed00c5656c49c12c45f7c2fa635d808be20df
Zone.N <Zone.Niuzh@hotmail.com>
2023-10-23 10:46:18 +0800

    feat: add api
    Signed-off-by: Zone.N <Zone.Niuzh@hotmail.com>

---
+++ src/include/SimpleRenderer.h
@@ -0,0 +1,25 @@
+
+/**
+ * @file SimpleRenderer.h
+ * @brief SimpleRenderer 接口头文件
+ * @author Zone.N (Zone.Niuzh@hotmail.com)
+ * @version 1.0
+ * @date 2023-10-23
+ * @copyright MIT LICENSE
+ * https://github.com/Simple-XX/SimpleRenderer
+ * @par change log:
+ * <table>
+ * <tr><th>Date<th>Author<th>Description
+ * <tr><td>2023-10-23<td>Zone.N<td>创建文件
+ * </table>
+ */
+
+#ifndef SIMPLERENDER_SIMPLERENDER_H
+#define SIMPLERENDER_SIMPLERENDER_H
+
+#include "framebuffer.h"
+#include "light.h"
+#include "render.h"
+#include "scene.h"
+
+#endif /* SIMPLERENDER_SIMPLERENDER_H */
